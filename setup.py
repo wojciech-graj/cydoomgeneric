d011f0d5770728c6beea35556aa66bb3767e485c
Lena Bertho <23459846+Baduit@users.noreply.github.com>
2024-10-21 20:35:00 +0200

    Add myself in the copyright at the top

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 """
  Copyright(C) 2023-2024 Wojciech Graj
+ Copyright(C) 2024 Lena Bertho
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
